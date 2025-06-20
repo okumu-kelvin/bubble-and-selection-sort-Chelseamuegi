a1d443d8b1321f832a16e57f329221c07a756aff
Chelsea Muegi <chelsea.muegi@strathmore.edu>
2025-06-20 11:30:16 +0300

    sorting logic

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,20 @@
 def bubble_sort(unsorted_list):
     # TODO: Implement bubble sort
-    pass
+    for i in range(len(unsorted_list)):
+        for j in range(0, len(unsorted_list) -i -1):
+
+            if unsorted_list[j] > unsorted_list[j + 1]:
+                unsorted_list[j], unsorted_list[j + 1] = unsorted_list[j + 1], unsorted_list[j]
+
+
+
+
+data = [-6,7,56,-2]
+
+bubble_sort(data)
+
+print('Sorted List: ')
+print(data)
+
+
+
