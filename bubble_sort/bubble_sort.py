a9827968bb5385116c79a89d138460db10102f35
Chelsea Muegi <chelsea.muegi@strathmore.edu>
2025-06-20 12:13:07 +0300

    WOr

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,13 +1,12 @@
 def bubble_sort(unsorted_list):
     # TODO: Implement bubble sort
-    for i in range(len(unsorted_list)):
+    for i in range(len(unsorted_list)-1):
         for j in range(0, len(unsorted_list) -i -1):
 
             if unsorted_list[j] > unsorted_list[j + 1]:
                 unsorted_list[j], unsorted_list[j + 1] = unsorted_list[j + 1], unsorted_list[j]
 
-
-
+    return unsorted_list
 
 data = [-6,7,56,-2]
 
--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,20 @@
 def selection_sort(arr):
+    n = len(arr)
+    for i in range(n-1):
+
+        min_idx = 1
+
+        for j in range(i+1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        if min_idx != i:
+
+            arr[j], arr[min_idx] = arr[min_idx], arr[j]
+
+    return arr
+
+
+
+
     # TODO: Implement selection sort
-    pass
+
