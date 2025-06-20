b756a693b229d60a08ff045060de98b30bb2122d
Chelsea Muegi <chelsea.muegi@strathmore.edu>
2025-06-20 12:15:21 +0300

    WOr

--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -2,13 +2,12 @@ def selection_sort(arr):
     n = len(arr)
     for i in range(n-1):
 
-        min_idx = 1
+        min_idx = i
 
         for j in range(i+1, n):
             if arr[j] < arr[min_idx]:
                 min_idx = j
         if min_idx != i:
-
             arr[j], arr[min_idx] = arr[min_idx], arr[j]
 
     return arr
